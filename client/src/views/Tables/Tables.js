17a320f8cc4e77041edd11cf652d92363554e20d
Satyam Pund <pundsatyam@gmail.com>
2023-01-25 19:05:52 +0530

    feat: book table button

--- client/src/views/Tables/Tables.js
+++ client/src/views/Tables/Tables.js
@@ -2,13 +2,10 @@ import React, { useState } from 'react';
 import './Tables.css';
 import axios from 'axios';
 import swal from 'sweetalert';
-import AvailableTable from './../../assests/available-table.png';
+import AvailableTable from './../../assests/table.png';
 
 const Tables = () => {
   const [availabeTable, setAvailabeTable] = useState([]);
-  // const [bookTable, setBookTable] = useState('');
-  // const [tableNumber, setTableNumber] = useState('');
-  // const [userId, setuserId] = useState('');
 
   async function fetchAvailabeTalbles() {
     console.log('fetching all tables');
@@ -44,23 +41,24 @@ const Tables = () => {
 
   return (
     <>
-      <button onClick={fetchAvailabeTalbles} className="btn btn-danger">
+      <button onClick={fetchAvailabeTalbles} className="btn btn-danger mx-3">
         Avalilabe Tables
       </button>
 
-      <div className="container">
+      <div className="container table-container">
         <div className="row text-center">
           {availabeTable?.map((table, index) => {
             return (
-              <div className="col-4">
-                <p>Table Number - {table.tableNumber}</p>
-                <img
-                  src={AvailableTable}
-                  className="table"
+              <div key={index} className="col-md-3 tableCard">
+                <p className="tableNumber">Table Number - {table.tableNumber}</p>
+                <img src={AvailableTable} className="table" alt="random" />
+                <br></br>
+                <button
+                  className="btn btn-danger"
                   id={[table.tableNumber, table._id]}
-                  alt="random"
-                  onClick={bookThisTable}
-                />
+                  onClick={bookThisTable}>
+                  Book Table
+                </button>
               </div>
             );
           })}
